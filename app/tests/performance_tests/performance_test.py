a1416f4210452a8900c85128c0130d82ff0e6278
Dina Nogueira <dinalivia2@gmail.com>
2025-01-08 00:12:11 +0100

    add realistic test

--- app/tests/performance_tests/performance_test.py
+++ app/tests/performance_tests/performance_test.py
@@ -7,12 +7,12 @@ class TestAlgorithmPerformance(unittest.TestCase):
     def test_time_complexity_square(self):
         plt.figure()
         plt.title("Algorithm Performance - square Path")
-        plt.xlabel("Commands + Steps")
+        plt.xlabel("Steps")
         plt.ylabel("Execution Duration (s)")
         plt.grid()
 
-        x_values_v2, y_values_v2 = [], []
-        x_values_v1, y_values_v1 = [], []
+        x_values_new, y_values_new = [], []
+        x_values_old, y_values_old = [], []
 
         for size in range(100):
             # testing multiple loops
@@ -27,44 +27,104 @@ class TestAlgorithmPerformance(unittest.TestCase):
                     ]
                 )
 
-            _, execution_time_new = execute_commands_v2(commands, 0, 0)
             _, execution_time_old = execute_commands_v1(commands, 0, 0)
+            _, execution_time_new = execute_commands_v2(commands, 0, 0)
+
             total_commands = len(commands)
             total_steps = sum(cmd["steps"] for cmd in commands)
-            x_values_v2.append(total_commands + total_steps)
-            y_values_v2.append(execution_time_new)
-            x_values_v1.append(total_commands + total_steps)
-            y_values_v1.append(execution_time_old)
+            x_values_new.append(total_steps)
+            y_values_new.append(execution_time_new)
+            x_values_old.append(total_steps)
+            y_values_old.append(execution_time_old)
 
         plt.plot(
-            x_values_v2,
-            y_values_v2,
-            marker="o",
-            linestyle="-",
+            x_values_old,
+            y_values_old,
+            marker="x",
+            linestyle="--",
             label="Algorithm V1",
         )
         plt.plot(
-            x_values_v1,
-            y_values_v1,
-            marker="x",
-            linestyle="--",
+            x_values_new,
+            y_values_new,
+            marker="o",
+            linestyle="-",
             label="Algorithm V2",
         )
         plt.legend()
-        plt.savefig(
-            "./app/tests/performance_tests/duration_vs_commands_steps_square.png"
-        )
+        plt.savefig("./duration_vs_commands_steps_square.png")
         print("Graph saved as 'duration_vs_commands_steps_square.png'")
 
-    def test_time_complexity_al_dente(self):
+    def test_time_complexity_realistic_path(self):
         plt.figure()
-        plt.title("Algorithm Performance - Dente Path")
-        plt.xlabel("Commands + Steps")
+        plt.title("Algorithm Performance - realistic Path")
+        plt.xlabel("Steps")
         plt.ylabel("Execution Duration (s)")
         plt.grid()
 
-        x_values_v2, y_values_v2 = [], []
-        x_values_v1, y_values_v1 = [], []
+        x_values_new, y_values_new = [], []
+        x_values_old, y_values_old = [], []
+
+        commands = []
+        last_time_v1 = 0
+        last_time_v2 = 0
+        while len(commands) < 3000:
+            print("number of commands", len(commands))
+
+            # for _ in range(size):
+            commands.extend(
+                [
+                    {"direction": "north", "steps": 100000},
+                    {"direction": "east", "steps": 1},
+                    {"direction": "south", "steps": 100000},
+                    {"direction": "east", "steps": 1},
+                ]
+            )
+
+            total_steps = sum(cmd["steps"] for cmd in commands)
+
+            if last_time_v1 <= 0.25:
+                _, execution_time_old = execute_commands_v1(commands, 0, 0)
+                last_time_v1 = execution_time_old
+                x_values_old.append(total_steps)
+                y_values_old.append(execution_time_old)
+
+            if last_time_v2 <= 0.25:
+                _, execution_time_new = execute_commands_v2(commands, 0, 0)
+                last_time_v2 = execution_time_new
+                x_values_new.append(total_steps)
+                y_values_new.append(execution_time_new)
+
+            if last_time_v1 > 0.25 and last_time_v2 > 0.25:
+                break
+
+        plt.plot(
+            x_values_old,
+            y_values_old,
+            marker="x",
+            linestyle="--",
+            label="Algorithm V1",
+        )
+        plt.plot(
+            x_values_new,
+            y_values_new,
+            marker="o",
+            linestyle="-",
+            label="Algorithm V2",
+        )
+        plt.legend()
+        plt.savefig("./duration_vs_commands_steps_realistic.png")
+        print("Graph saved as 'duration_vs_commands_steps_realistic.png'")
+
+    def test_time_complexity_al_dente(self):
+        plt.figure()
+        plt.title("Algorithm Performance - dente Path")
+        plt.xlabel("Steps")
+        plt.ylabel("Execution Duration (s)")
+        plt.grid()
+
+        x_values_new, y_values_new = [], []
+        x_values_old, y_values_old = [], []
 
         for size in range(500):
             commands = []
@@ -78,33 +138,32 @@ class TestAlgorithmPerformance(unittest.TestCase):
                     ]
                 )
 
-            _, execution_time_new = execute_commands_v2(commands, 0, 0)
             _, execution_time_old = execute_commands_v1(commands, 0, 0)
+            _, execution_time_new = execute_commands_v2(commands, 0, 0)
+
             total_commands = len(commands)
             total_steps = sum(cmd["steps"] for cmd in commands)
-            x_values_v2.append(total_commands + total_steps)
-            y_values_v2.append(execution_time_new)
-            x_values_v1.append(total_commands + total_steps)
-            y_values_v1.append(execution_time_old)
+            x_values_new.append(total_steps)
+            y_values_new.append(execution_time_new)
+            x_values_old.append(total_steps)
+            y_values_old.append(execution_time_old)
 
         plt.plot(
-            x_values_v2,
-            y_values_v2,
-            marker="o",
-            linestyle="-",
+            x_values_old,
+            y_values_old,
+            marker="x",
+            linestyle="--",
             label="Algorithm V1",
         )
         plt.plot(
-            x_values_v1,
-            y_values_v1,
-            marker="x",
-            linestyle="--",
+            x_values_new,
+            y_values_new,
+            marker="o",
+            linestyle="-",
             label="Algorithm V2",
         )
         plt.legend()
-        plt.savefig(
-            "./app/tests/performance_tests/duration_vs_commands_steps_dente.png"
-        )
+        plt.savefig("./duration_vs_commands_steps_dente.png")
         print("Graph saved as 'duration_vs_commands_steps_dente.png'")
 
 
